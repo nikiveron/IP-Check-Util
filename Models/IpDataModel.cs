b59ecde99482a41d941be304f6d57cfd8abd83af
nikiveron <nikiveron@bk.ru>
2026-02-05 00:30:17 +0400

    added IpDataModel

---
+++ Models/IpDataModel.cs
@@ -0,0 +1,16 @@
+﻿
+namespace IPCheckUtil.Models;
+
+public class IpDataModel
+{
+    public string Ip { get; set; } = string.Empty;
+    public string Hostname { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+    public string Region { get; set; } = string.Empty;
+    public string Country { get; set; } = string.Empty;
+    public string Loc { get; set; } = string.Empty; 
+    public string Org { get; set; } = string.Empty;
+    public string Postal { get; set; } = string.Empty;
+    public string Timezone { get; set; } = string.Empty;
+    public string Readme { get; set; } = string.Empty;
+}
